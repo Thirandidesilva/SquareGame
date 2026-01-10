3a598d2ef1ae7475e477aef7af52fb61f4f03f1f
Thirandidesilva <thirandidesilva2002@gmail.com>
2026-01-10 11:42:21 +0530

    Initial Commit

---
+++ SquareGame/SquareGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquareGameApp.swift
+//  SquareGame
+//
+//  Created by Thirandi De Silva on 2026-01-10.
+//
+
+import SwiftUI
+
+@main
+struct SquareGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
