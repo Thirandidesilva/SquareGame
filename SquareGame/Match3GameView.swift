e9e4d05be3fb727a71a8194cf9c954a9bc64f098
Thirandidesilva <thirandidesilva2002@gmail.com>
2026-01-21 10:13:02 +0530

    Created Match3GameView swiftui file

---
+++ SquareGame/Match3GameView.swift
@@ -0,0 +1,18 @@
+//
+//  Match3GameView.swift
+//  SquareGame
+//
+//  Created by Thirandi De Silva on 2026-01-21.
+//
+
+import SwiftUI
+
+struct Match3GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Match3GameView()
+}
